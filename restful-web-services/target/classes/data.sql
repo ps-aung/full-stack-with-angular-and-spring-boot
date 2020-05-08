e9d844e68ed0acf570fbe44ec9e1a1789a8b39ca
Pyae Sone Aung <pyaesoneaung.ucsy@gmail.com>
2020-05-08 15:41:30 +0630

    first commit

---
+++ restful-web-services/target/classes/data.sql
@@ -0,0 +1,8 @@
+insert into todo(id, username,description,target_date,is_done)
+values(10001, 'pyaesoneaung', 'Learn JPA', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10002, 'pyaesoneaung', 'Learn Data JPA', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10003, 'pyaesoneaung', 'Learn Microservices', sysdate(), false);
